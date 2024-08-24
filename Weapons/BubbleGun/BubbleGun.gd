1aed3b9c4e9481c380a077ad116c9123918783bd
fraxouille <fraxouille@hotmail.com>
2024-08-24 18:27:56 +0200

    Gun

--- Weapons/BubbleGun/BubbleGun.gd
+++ Weapons/BubbleGun/BubbleGun.gd
@@ -3,9 +3,14 @@ extends Node2D
 @onready var timer: Timer = $Timer
 @onready var weapons = %Weapons
 @onready var bubble_constructor = preload("res://Weapons/BubbleGun/Bubble.tscn")
+@onready var sprite = %AnimatedSprite2D
+@onready var sprite_timer: Timer = %AnimatedSprite2D/SpriteTimer
 
 var nb_weapon_activated: int = 1
 var can_shoot: bool = false
+
+func _ready():
+	sprite.frame = 0
 
 func _physics_process(delta: float) -> void:
 	if can_shoot and timer.is_stopped():
@@ -19,6 +24,7 @@ func _input(event):
 		can_shoot = false
 		
 func shoot():
+	change_sprite()
 	for i in range(nb_weapon_activated):
 		var bubble = bubble_constructor.instantiate()
 		bubble.global_position = weapons.get_child(i).global_position
@@ -38,3 +44,10 @@ func get_multiplicator_angle(pos: int):
 		return 0
 	else:
 		return 1
+
+func change_sprite():
+	sprite.frame += 1
+	sprite_timer.start()
+	
+func _on_sprite_timer_timeout():
+	sprite.frame -= 1
