f2a1628fba6ffedce1106808b114f6eddaa679a0
fraxouille <fraxouille@hotmail.com>
2025-01-12 18:18:15 +0100

    Réorganisation Shield et armes

--- Weapons/Turtle/Shield/Shield.gd
+++ Weapons/Turtle/Shield/Shield.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var collision_shape: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var shield_generator: AnimatedSprite2D = $ShieldGenerator
 @export var character_collision_shape: CollisionShape2D
 @export var shield_life: int = 2
+@export var maxLevel : int = 1;
 
+var level : int = 0;
 var shield_value: int = 2
 var is_activate: bool = false:
 	set(value):
@@ -12,11 +15,17 @@ var is_activate: bool = false:
 			shield_value += shield_life
 			return
 		sprite.visible = value
-		character_collision_shape.disabled = value
+		#character_collision_shape.disabled = value
 		collision_shape.disabled = !shield_value
 
 func _ready():
 	is_activate = false
+	shield_generator.visible = false
+
+func add_weapon():
+	if level < maxLevel:
+		level += 1
+		shield_generator.visible = true
 
 func _on_area_2d_body_entered(body):
 	shield_value -= 1
