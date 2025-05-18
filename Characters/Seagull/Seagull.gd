cf946de1fc2d60580ce68e6bd4ff48da95b7adfb
agoryu <elliot.vanegue@protonmail.com>
2025-05-18 14:50:59 +0200

    feat: ajout de l'animation des moteurs des mouettes

--- Characters/Seagull/Seagull.gd
+++ Characters/Seagull/Seagull.gd
@@ -3,8 +3,12 @@ extends CharacterBody2D
 @export var speed = 200.0
 @export var drag := 5.0
 @export var damage := 1
+@export var fire_scale = 1.0
 
 @onready var health: Health = $Health
+@onready var engine = $Engine
+
+var fire_step = 0.1
 
 func _physics_process(delta):
 	var orientation = Vector2.DOWN if rotation_degrees == -180 else Vector2.UP
@@ -13,6 +17,7 @@ func _physics_process(delta):
 	velocity += steering / drag
 	velocity = velocity.limit_length(speed)
 	move_and_slide()
+	anim_engine()
 
 func _on_timer_timeout():
 	queue_free()
@@ -26,3 +31,13 @@ func _on_hit_box_body_entered(body: Node2D) -> void:
 
 func _on_health_dead() -> void:
 	queue_free()
+
+func anim_engine():
+	if engine and speed > 0:
+		if (fire_scale > 1.4 and fire_step > 0):
+			fire_step = -0.05
+		elif (fire_scale < 0.9 and fire_step < 0):
+			fire_step = 0.05
+		fire_scale += fire_step
+		for fire in engine.get_children():
+			fire.scale = Vector2.ONE * 0.05 * fire_scale
