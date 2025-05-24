fff74240095105aaa45da588676547000e459a6b
agoryu <elliot.vanegue@protonmail.com>
2025-05-24 12:48:47 +0200

    feat: mega bulle octopus

---
+++ Characters/Octopus/MegaBubble/MegaBubble.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@onready var health : Health = $Health
+@export var speed = 50.0
+@export var damage = 5
+
+func _physics_process(delta):
+	var direction = Vector2(sin(rotation), -cos(rotation))
+	move_and_collide(direction.normalized() * speed * delta)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if "damage" in body:
+		health.loose_health(body.damage)
+		body.queue_free()
+
+func _on_health_dead() -> void:
+	queue_free()
--- Characters/Octopus/Octopus.gd
+++ Characters/Octopus/Octopus.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @onready var howl_scene : Howl = $Howl
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 @onready var area_damage : Area2D = $Area2D
+@onready var attack_position : Marker2D = $AttackPosition
+
+@onready var attack_constructor = preload("res://Characters/Octopus/MegaBubble/MegaBubble.tscn")
 
 @export var life : ProgressBar
 
@@ -14,3 +17,10 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	life.value -= body.damage
 	body.queue_free()
 	animation_player.play("take_damage")
+
+
+func _on_attack_timer_timeout() -> void:
+	var mega_bubble = attack_constructor.instantiate() as CharacterBody2D
+	mega_bubble.global_position = attack_position.global_position
+	mega_bubble.global_rotation_degrees = -90
+	get_tree().root.add_child(mega_bubble)
