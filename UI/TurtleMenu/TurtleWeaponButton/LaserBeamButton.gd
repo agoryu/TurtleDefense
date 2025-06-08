8811a33eef83c3325474a12e63a9649e128ab21b
agoryu <elliot.vanegue@protonmail.com>
2025-06-08 17:57:17 +0200

    fix: problème ui

--- UI/TurtleMenu/TurtleWeaponButton/LaserBeamButton.gd
+++
@@ -1,5 +0,0 @@
-extends TurtleWeaponButton
-
-func _ready() -> void:
-	weapon = Game.turtle.beam_weapon
-	$Weapon/Details.cost_label.text = "%d" % weapon.cost_weapon
--- UI/TurtleMenu/TurtleWeaponButton/UrsinCanonButton.gd
+++
@@ -1,5 +0,0 @@
-extends TurtleWeaponButton
-
-func _ready() -> void:
-	weapon = Game.turtle.ursin_canon_weapon
-	$Weapon/Details.cost_label.text = "%d" % weapon.cost_weapon
